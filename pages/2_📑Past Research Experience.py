21940e408278f820549a472cdcca502abc100c18
YZhang0328 <yujia.zhang.uom@gmail.com>
2025-03-16 23:20:23 +0000

    Make indentation to my research experience

--- pages/2_📑Past Research Experience.py
+++ pages/2_📑Past Research Experience.py
@@ -22,11 +22,11 @@ with st.container():
             <li style="font-size: 18px;">Developed an optimization framework for wave energy converters (WECs) to minimize its levelized cost of electricity (LCOE) via mixed integer nonlinear programming (MINLP) and robust optimization theory.</li>
             <li style="font-size: 18px;">Conduct the parameter sensitivity analysis to system performance, i.e., mining parameters that can enhance WEC productivity and efficiency, while reducing LCoE.</li>
             <li style="font-size: 18px;">Representative publications:</li>
-            <ol style="font-size: 18px;">
-                <li style="text-indent: -25px; padding-left: 25px;">Yujia Zhang and Guang Li. Robust tube-based model predictive control for wave energy converters. IEEE Transactions on Sustainable Energy (2022).</li>
-                <li style="text-indent: -25px; padding-left: 25px;">Yujia Zhang, Hongbiao Zhao, Guang Li, Christopher Edwards, and Mike Belmont. Robust nonlinear model predictive control of an autonomous launch and recovery system. IEEE Transactions on Control Systems Technology (2023).</li>
-                <li style="text-indent: -25px; padding-left: 25px;">Yujia Zhang, Guang Li and Mustafa Al-Ani. Robust Learning-based Model Predictive Control for Wave Energy Converters. IEEE Transactions on Sustainable Energy (2024).</li>
-                <li style="text-indent: -25px; padding-left: 25px;">Yujia Zhang and Guang Li. Towards Robust and High-performance Operations of Wave Energy Converters: an Adaptive Tube-based Model Predictive Control Approach. IFAC-PapersOnLine, 55(31):339-344, 2022.</li>
+            <ol style="font-size: 18px; padding-left: 30px;">
+                <li style="margin-left: 15px;">Yujia Zhang and Guang Li. Robust tube-based model predictive control for wave energy converters. IEEE Transactions on Sustainable Energy (2022).</li>
+                <li style="margin-left: 15px;">Yujia Zhang, Hongbiao Zhao, Guang Li, Christopher Edwards, and Mike Belmont. Robust nonlinear model predictive control of an autonomous launch and recovery system. IEEE Transactions on Control Systems Technology (2023).</li>
+                <li style="margin-left: 15px;">Yujia Zhang, Guang Li and Mustafa Al-Ani. Robust Learning-based Model Predictive Control for Wave Energy Converters. IEEE Transactions on Sustainable Energy (2024).</li>
+                <li style="margin-left: 15px;">Yujia Zhang and Guang Li. Towards Robust and High-performance Operations of Wave Energy Converters: an Adaptive Tube-based Model Predictive Control Approach. IFAC-PapersOnLine, 55(31):339-344, 2022.</li>
             </ol>
         </ul>
         """, 
