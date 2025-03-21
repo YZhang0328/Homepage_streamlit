0181d27137f5546b2b24c755502719442b9ee877
YZhang0328 <yujia.zhang.uom@gmail.com>
2025-03-21 21:55:38 +0000

    Make the postdoc experience inside the container

--- pages/2_📑Past Research Experience.py
+++ pages/2_📑Past Research Experience.py
@@ -35,12 +35,6 @@ with st.container():
             unsafe_allow_html=True
         )
 
-    with image_column:
-        st.image("images/background_picture.png")
-
-
-with st.container():
-        
         st.markdown(
             """
             <ul>
@@ -50,6 +44,12 @@ with st.container():
             """, 
             unsafe_allow_html=True
         )
+        
+    with image_column:
+        st.image("images/background_picture.png")
+
+
+with st.container():
                 
         st.markdown(
             '<p style="font-size: 18px;"> During my PhD, I worked on <b>data-driven renewable energy system modelling and control</b>, where I\'ve </p>',
