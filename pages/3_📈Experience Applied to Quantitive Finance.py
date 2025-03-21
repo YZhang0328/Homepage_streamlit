c13faf9bd7fbfa1914518ceaeb142ba3db9c2c20
YZhang0328 <yujia.zhang.uom@gmail.com>
2025-03-21 11:23:59 +0000

    Added one more page

---
+++ pages/3_📈Experience Applied to Quantitive Finance.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+with st.container():
+    st.markdown(
+        """
+        <h3><i>Modern trading and asset allocation increasingly relies on scientific and quantitative methodologies, rather than traditional experience-based approaches.</i></h3>
+    
+        <h3><i>In trading and asset allocation, data-based forecasting and mathematical formulation are pivotal, offering a structured and efficient way to predict the future price trend assisting the decision-making progress, optimize portfolios and manage risks while adhering to regulatory requirements. Developing effective investment strategies requires analyzing financial data, making prediction, formulating optimization problems to balance competing objectives—maximizing returns and minimizing risk within specified constraints. </i></h3>
+        """, 
+        unsafe_allow_html=True
+    )
+
+ 
