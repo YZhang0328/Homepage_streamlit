ac8ff0ad46645cc7b72e3c4ce12ece01520276ed
YZhang0328 <yujia.zhang.uom@gmail.com>
2025-03-21 21:59:36 +0000

    Added container

--- pages/2_📑Past Research Experience.py
+++ pages/2_📑Past Research Experience.py
@@ -44,13 +44,7 @@ with st.container():
             """, 
             unsafe_allow_html=True
         )
-        
-    with image_column:
-        st.image("images/background_picture.png")
 
-
-with st.container():
-                
         st.markdown(
             '<p style="font-size: 18px;"> During my PhD, I worked on <b>data-driven renewable energy system modelling and control</b>, where I\'ve </p>',
             unsafe_allow_html=True
@@ -66,6 +60,9 @@ with st.container():
             """, 
             unsafe_allow_html=True
         )
+
+    with image_column:
+        st.image("images/background_picture.png")
 
 
 st.write("---")
